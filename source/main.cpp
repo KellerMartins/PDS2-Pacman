2bea2faf70e80e0b4a80e04a3348d674c6b5a388
Keller Clayderman <keller_clayderman@hotmail.com>
2018-10-10 23:18:53 -0300

    Adicionado teste de animação

--- source/main.cpp
+++ source/main.cpp
@@ -17,7 +17,22 @@ int main(){
     Object3D smallGrid("assets/models/Plane.obj", DARKGRAY);
     Mapa m("assets/maps/mapa_exemplo.txt");
     
+    Object3D pacman("assets/models/pacman/walk/player_walk_0.obj", BLUE);
+    pacman.position = (Vector3){3,0,10};
+    std::string walk[] = {"assets/models/pacman/walk/player_walk_0.obj",
+                          "assets/models/pacman/walk/player_walk_1.obj",
+                          "assets/models/pacman/walk/player_walk_2.obj",
+                          "assets/models/pacman/walk/player_walk_3.obj",
+                          "assets/models/pacman/walk/player_walk_4.obj",
+                          "assets/models/pacman/walk/player_walk_5.obj"};
+
+    float animTime = 0;
+
     while(!WindowShouldClose()){
+        
+        pacman.Load3DModel(walk[(int)animTime]);
+        animTime += GetFrameTime() * 9/*Frames por segundo*/;
+        animTime = (int)animTime > 5/*Num frames*/ ? 0 : animTime;
 
         GameEvents::TriggerUpdate();
 
