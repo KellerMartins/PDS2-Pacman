6b205c79e0931215d2575d7e9dd6a96e8c9574dc
Rennan Cordeiro <rennancl@gmail.com>
2018-10-19 15:09:22 -0300

    Classe pacman upada

--- source/Pacman.h
+++ source/Pacman.h
@@ -4,13 +4,19 @@
 #include <string>
 
 class Pacman : public Personagem {
-public:
+private:
 	int pontuacao = 0;
-	Pacman();
+	int vidas;
+	int direcao_y;
+	int direcao_x;
+public:
+	Pacman(int x, int y);
 	void calcula_direcao();
-	void mover();
+	void mover(vector<enemy> enemies);
+	int verifica_posicao(vector<enemy> enemies);
 	void colisao();
 	void morrer();
+	void OnUpdate();
 };
 
 
